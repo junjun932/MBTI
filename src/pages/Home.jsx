39c0d464745eaacdb322d0dfdfc5dced0f1c470e
junjun932 <1004ji93@gmail.com>
2024-09-11 15:40:37 +0900

    edit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,12 @@
-import React from "react";
 import { Link } from "react-router-dom";
+import React from "react";
 
 const Home = () => {
   return (
     <div>
-      <h1>
-        이게 최종 찐 막 MBTI 테스트 
-      </h1>
-      <p>
-        아묻따 선택 ㄱ ㄱ 
-      </p>
-      <Link to=???>
-        로그인하기
-      </Link>
+      <h1>이게 최종 찐 막 MBTI 테스트</h1>
+      <p>아묻따 선택 ㄱ ㄱ</p>
+      <Link to="/src/pages/Login.jsx">로그인하기</Link>
     </div>
   );
 };
