675c24d056e0644c0a6d33328c9c9eb2bb11cf61
jun <1004ji93@gmail.com>
2024-10-15 02:18:51 +0900

    API CRUD

--- src/pages/TestResultPage.jsx
+++ src/pages/TestResultPage.jsx
@@ -1,14 +1,26 @@
 import axios from "axios";
+//crud
 
-const API_URL = "http://localhost:5000/testResults";
+const API_URL = "http://localhost:5001/testResults";
 
 export const getTestResults = async () => {
   const response = await axios.get(API_URL);
   return response.data;
 };
 
-export const createTestResult = async (resultData) => {};
+export const createTestResult = async (resultData) => {
+  const response = await axios.post(API_URL, resultData);
+  return response.date;
+};
 
-export const deleteTestResult = async (id) => {};
+export const deleteTestResult = async (id) => {
+  const response = await axios.delete(`${API_URL}/${id}`);
+  return response.data;
+};
 
-export const updateTestResultVisibility = async (id, visibility) => {};
+export const updateTestResultVisibility = async (id, visibility) => {
+  const response = await axios.patch(`${API_URL}/${id}`, {
+    visibility: visibility,
+  });
+  return response.data;
+};
